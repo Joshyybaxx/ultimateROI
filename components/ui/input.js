ac532f25fc7cd4a0db7b69a5591afe7efd53c2b1
Joshyybaxx <josh@compdemo.net>
2025-05-23 19:32:24 +1000

    Update input.js

--- components/ui/input.js
+++ components/ui/input.js
@@ -1,3 +1,13 @@
-export function Input({ type = "text", name, value, onChange, className }) {
-  return <input type={type} name={name} value={value} onChange={onChange} className={`border rounded px-2 py-1 w-full ${className}`} />;
-}
+import * as React from "react";
+
+export const Input = React.forwardRef(({ className = "", ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={`w-full rounded-md border border-gray-700 bg-gray-900 text-white placeholder-gray-400 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+      {...props}
+    />
+  );
+});
+
+Input.displayName = "Input";
