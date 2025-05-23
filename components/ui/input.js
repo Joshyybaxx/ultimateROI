69bf667b55e76bd8b59b72dd24b7704c82474bee
Joshyybaxx <josh@compdemo.net>
2025-05-23 19:41:20 +1000

    Update input.js

--- components/ui/input.js
+++ components/ui/input.js
@@ -1,11 +1,12 @@
 import * as React from "react";
+import { cn } from "./utils"; // if you don't use clsx/twMerge, remove this line
 
 export const Input = React.forwardRef(({ className = "", ...props }, ref) => {
   return (
     <input
       ref={ref}
       {...props}
-      className={`w-full bg-gray-900 text-white placeholder-gray-400 border border-gray-700 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+      className={`w-full bg-gray-900 text-white placeholder-gray-400 border border-gray-600 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
     />
   );
 });
