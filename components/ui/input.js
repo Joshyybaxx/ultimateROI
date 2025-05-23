9df8d15d22fd854d086b292349759243668c846b
Joshyybaxx <josh@compdemo.net>
2025-05-23 19:37:30 +1000

    Update input.js

--- components/ui/input.js
+++ components/ui/input.js
@@ -4,7 +4,7 @@ export const Input = React.forwardRef(({ className = "", ...props }, ref) => {
   return (
     <input
       ref={ref}
-      className={`w-full rounded-md border border-gray-700 bg-gray-900 text-white placeholder-gray-400 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+      className={`w-full bg-gray-900 text-white placeholder-gray-400 border border-gray-700 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
       {...props}
     />
   );
